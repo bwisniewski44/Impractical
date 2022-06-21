cc64f7d6b4c2ffaf11ae43321f483362898f8a3d
Brian Wisniewski <brianwisniewski44@gmail.com>
2022-06-21 07:05:24 -0400

    Implement functions by which to read files, accept user input

--- Chapter 12/nest_egg_mcs.py
+++ Chapter 12/nest_egg_mcs.py
@@ -1,11 +1,71 @@
 """
-TODO EXPLAIN
+Implements the Monte Carlo Simulation (MCS) described by Chapter 12.
+
+TODO EXPLAIN MORE
+
+Source files:
+https://www.nostarch.com/impracticalpython
 """
+
+import sys
+import random
+import matplotlib.pyplot as plt
+import typing
+
+
+def read_percentages(path):
+    """
+    TODO EXPLAIN
+
+    :param str path:
+
+    :return:
+    :rtype: list[float]
+    """
+
+    # We'll collect the results here
+    values = []  # type: typing.List[float]
+
+    # Collect the lines of the file
+    with open(path) as infile:
+        lines = infile.readlines()
+
+    # For each line...
+    for line in lines:
+        # ... resolve the decimal version of the expressed percentage
+        percentage_expression = line.strip()
+        decimal_value = float(percentage_expression) / 100.0
+
+        # Round the value and append to the result
+        decimal_value = round(decimal_value, 5)
+        values.append(decimal_value)
+
+    return values
+
+
+def default_input(prompt, default=None):
+    """
+    TODO EXPLAIN
+
+    :param str prompt:
+    :param str default:
+
+    :return:
+    :rtype: str | None
+    """
+    if default:
+        prompt = prompt + f" [{default}]"
+    prompt += ": "
+
+    response = input(prompt).strip()
+    if not response:
+        response = default
+    return response
 
 
 def run():
     """
-    TODO EXPLAIN
+    Main function of this script. TODO EXPLAIN MORE
 
     :return: None
     """
